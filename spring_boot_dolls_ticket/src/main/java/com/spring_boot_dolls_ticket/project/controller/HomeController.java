1a2bdedc952a3a71b1600a64347c853944097497
bbidduri924 <bbidduri924@gmail.com>
2024-09-27 14:06:20 +0900

    first commit

---
+++ spring_boot_dolls_ticket/src/main/java/com/spring_boot_dolls_ticket/project/controller/HomeController.java
@@ -0,0 +1,34 @@
+package com.spring_boot_dolls_ticket.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index() {
+		
+		return "index";
+		
+	}
+	@RequestMapping("/csAdmin")
+	public String adminPage() {
+		
+		return "customerService/csAdmin";
+		
+	}
+	@RequestMapping("/myPage")
+	public String myPage() {
+		
+		return "member/myPage";
+		
+	}
+	@RequestMapping("/orderDetail")
+	public String orderDetail() {
+		
+		return "member/orderDetail";
+		
+	}
+
+}
