5adaec101957486f64b59a3ad7bd6f53501e6f14
bbidduri924 <bbidduri924@gmail.com>
2024-11-01 11:44:22 +0900

    경로 접근 권한 설정

--- spring_boot_dolls_ticket/src/main/java/com/spring_boot_dolls_ticket/project/WebConfig.java
+++ spring_boot_dolls_ticket/src/main/java/com/spring_boot_dolls_ticket/project/WebConfig.java
@@ -1,17 +1,28 @@
 package com.spring_boot_dolls_ticket.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
+	@Autowired
+	private AdminInterceptor adminInterceptor;
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// 프로젝트 외부 경로 이미지 맵핑 설정
 		registry.addResourceHandler("/image/**")
 				.addResourceLocations("classpath:/static/image/");
 	}
+	
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns("/admin/**"); // /admin 경로에 대해 인터셉터 적용
+    }
 	
 }
